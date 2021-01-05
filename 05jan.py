203e2aeb756e4ae5c43156560ac89985a82349c7
nerdishhomosapein <kumarprateek902@gmail.com>
2021-01-05 12:48:24 +0530

    5th Jan Answer

---
+++ 05jan.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+    def add_to_list(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+
+def merge_two_linked_list(head1, head2):
+    dummy_node = Node(0)
+    tail = dummy_node
+    while True:
+        if head1 is None:
+            tail.next = head2
+            break
+        if head2 is None:
+            tail.next = head1
+            break
+        if head1.data <= head2.data:
+            tail.next = head1
+            head1 = head1.next
+        else:
+            tail.next = head2
+            head2 = head2.next
+        tail = tail.next
+    return dummy_node.next
